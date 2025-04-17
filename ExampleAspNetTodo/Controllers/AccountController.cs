15fa35bb5bef932f7f1fafae9c06f0bbb55920bf
Krindendo work <lollipopfunkyx9@gmail.com>
2025-04-17 23:09:23 +0200

    Fixed style and added font

---
+++ ExampleAspNetTodo/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ExampleAspNetTodo.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
