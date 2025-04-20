633b6ee6a0e2b69b5db81119f45966bdd4e1dd6c
Krindendo work <lollipopfunkyx9@gmail.com>
2025-04-20 13:40:32 +0200

    Add post method for login

--- ExampleAspNetTodo/Controllers/AccountController.cs
+++ ExampleAspNetTodo/Controllers/AccountController.cs
@@ -1,12 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ExampleAspNetTodo.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ExampleAspNetTodo.Controllers
 {
     public class AccountController : Controller
     {
+        [HttpGet]
         public IActionResult Login()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            if (model.Email == "admin" && model.Password == "password123")
+            {
+                // Normally set authentication cookie here
+                return RedirectToAction("Index", "Home");
+            }
+
+            ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+            return View(model);
         }
     }
 }
---
+++ ExampleAspNetTodo/Models/LoginViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ExampleAspNetTodo.Models
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Email is required")]
+        [DataType(DataType.EmailAddress)]
+        public required string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        public required string Password { get; set; }
+    }
+}
